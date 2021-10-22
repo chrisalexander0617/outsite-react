e778e51be6659608ab8316427c798039cd208741
Chris <44784345+chrisalexander0617@users.noreply.github.com>
2021-10-21 20:26:55 -0400

    Added server with json data

--- server/index.js
+++ server/index.js
@@ -2,14 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const properties = require('./data/outsite_properties.json')
+
 const app = express();
 const port = process.env.port || 5000
+
+
+console.log(properties)
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (rq, rs) => {
-    rs.status(200).json([{message:'success'}])
+    rs.status(200).json(properties)
 })
 
 
