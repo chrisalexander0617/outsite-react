8a2c380598a3d6d36a4c014d2aeaa87bf53493c9
Chris <44784345+chrisalexander0617@users.noreply.github.com>
2021-10-23 20:33:20 -0400

    Added hooks, map properties created, array of cards form json data

--- src/components/content/LocationsGrid.js
+++ src/components/content/LocationsGrid.js
@@ -1,21 +1,44 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import LocationCard from '../cards/LocationCard';
 import axios from 'axios';
 
 
 function LocationsGrid() {
+    const [ properties, setProperties ] = useState();
 
-    axios.get('http://localhost:5000/').then(res => console.log(res.data.properties))
+
+    useEffect(() => {
+        fetchPropertiesData()
+    },[])
+
+    const url = 'http://localhost:5000/';
+
+    const fetchPropertiesData = () => {
+        axios.get(url)
+        .then(res => {
+            const allProperties = res.data.properties;
+            console.log(allProperties);
+            setProperties(allProperties)
+        })
+        .catch(err => console.log(err))
+    }
+
+    if(properties){
+        const locationCards = properties.map((property, i) => (<LocationCard key={i} img={property.wfContent.cityImage} />))
+
+        return (
+            <>
+                <div className="grid grid-cols-1 md:grid-cols-3">
+                    {locationCards}
+                </div>
+            </>
+        )
+    } else {
+        return (
+            <div>No Properties yet</div>
+        )
+    }
     
-    return (
-        <>
-            <div className="grid grid-cols-1 md:grid-cols-3">
-                <LocationCard />
-                <LocationCard />
-                <LocationCard />
-            </div>
-        </>
-    )
 }
 
 export default LocationsGrid
